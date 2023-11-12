dd3453d5ccea1d0841de14f14b88cd80769d638f
Sreepathy Jayanand <sreepathy0101@gmail.com>
2023-11-12 22:38:58 +0530

    init

---
+++ build/src/CMakeFiles/lib_hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib_hello_world.dir/print_helper.cpp.o"
+  "CMakeFiles/lib_hello_world.dir/print_helper.cpp.o.d"
+  "liblib_hello_world.a"
+  "liblib_hello_world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib_hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
